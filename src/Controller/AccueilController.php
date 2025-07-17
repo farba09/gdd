a01ea0dcc0dc181474ec7620c0d55a509ec95fc6
farba09 <pisco7155@gmail.com>
2025-07-18 00:23:51 +0200

    mise en place des données générées manuellement dand la page AcceuilController

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -11,11 +11,35 @@ final class AccueilController extends AbstractController
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
-
-
+        $signalements = [
+            [
+                'prenom' => 'Farba',
+                'nom' => 'KANE',
+                'email' => 'farba@kane.com',
+                'message' => 'je suis un message de farba',
+                'signalAdresse' => [
+                    'rue' => '83 rue de la ville',
+                    'codePostal' => '75001',
+                    'ville' => 'Paris',
+                    'picture' => 'https://randomcity.net/wp-content/uploads/2024/06/4095_juizdefora_Brazil-1024x768.jpg'
+                ]
+            ],
+            [
+                'prenom' => 'Maymuna',
+                'nom' => 'GAMBONI',
+                'email' => 'maymuna@gamboni.com',
+                'message' => 'je suis un message de maymuna',
+                'signalAdresse' => [
+                    'rue' => '83 rue de la ville',
+                    'codePostal' => '69001',
+                    'ville' => 'Lyon',
+                    'picture' => 'https://randomcity.net/wp-content/uploads/2024/06/4095_juizdefora_Brazil-1024x768.jpg'
+                ]
+            ]
+        ];
 
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'signalement' => $signalements
         ]);
     }
 }
